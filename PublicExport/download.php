52abb17b0961d015779267fffad5884b745351b0
Sainan <sainan@calamity.gg>
2023-12-13 19:16:25 +0100

    Fix PublicExport/download.php

--- PublicExport/download.php
+++ PublicExport/download.php
@@ -9,7 +9,7 @@ foreach (scandir(".") as $file)
 			$line = str_replace("\r", "", $line);
 			$name = explode("!", $line)[0];
 			echo "Downloading $name...\n";
-			$data = file_get_contents("http://content.warframe.com/PublicExport/Manifest/".$line);
+			$data = file_get_contents("https://content.warframe.com/PublicExport/Manifest/".$line);
 			$data = str_replace("\\r\r\n", "\\n", $data);
 			$data = json_encode(json_decode($data, true), JSON_PRETTY_PRINT | JSON_UNESCAPED_SLASHES);
 			file_put_contents($name, $data);
